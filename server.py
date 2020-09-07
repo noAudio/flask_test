e954943cae29bdfc2c76909454b1b46d1eb79de2
Samuel Odongo <samieodd@gmail.com>
2020-09-07 17:35:10 +0300

    Added function to write to csv

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
+import csv
+from csv import QUOTE_MINIMAL
 from flask import Flask, redirect
 from flask.globals import request
 from flask.templating import render_template
+from jinja2.lexer import newline_re
 
 app = Flask(__name__)
 
@@ -12,11 +15,22 @@ def my_home():
 def html_page(page_name):
     return render_template(page_name)
 
+def write_to_csv(data):
+    with open('database.csv', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, newline='')
+        csv_writer.writerow([email, subject, message])
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-        data = request.form.to_dict()
-        print(data)
-        return redirect('/thankyou.html')
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'Unable to save to database!'
     else:
         return 'Something went wrong...'
