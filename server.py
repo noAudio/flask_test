60c8fce58919699ede2974264632914194552f60
Samuel Odongo <samieodd@gmail.com>
2020-09-07 17:17:01 +0300

    Initial commit

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect
+from flask.globals import request
+from flask.templating import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'Something went wrong...'
